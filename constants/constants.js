5225ca5f2bfd76f21ca374bb13605c2cf77daa71
ozgurtutarr <ozgurtutarr@gmail.com>
2022-11-04 23:56:18 +0300

    first commit

---
+++ constants/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  '0x044A30609027cADb0103Cf18c2aBf4FB679E4363';
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
